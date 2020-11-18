07fca85686ad352d144633e76132af745bbc68a8
Yoni Leitersdorf <y@indeni.com>
2020-11-18 10:09:54 -0800

    Hmmm something failed, trying to simplify the flow with a one step init-and-plan

---
+++ cloudrailjson_to_gitlabsastjson.py
@@ -0,0 +1,7 @@
+# Convert this Cloudrail's results format
+# Into this:
+# https://docs.gitlab.com/12.10/ee/user/application_security/sast/#reports-json-format
+
+import json
+from jsonbender import bend, K, S
+
